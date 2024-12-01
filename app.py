dba7b57f1a76f868328c557e45e8e1894dc95af3
SriramR17 <sriramr958@gmail.com>
2024-12-01 13:49:38 +0530

    passwordhidden

--- app.py
+++ app.py
@@ -13,8 +13,10 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.getenv("EMAIL_USERNAME") # Replace with your email
 app.config['MAIL_PASSWORD'] = os.getenv("EMAIL_PASSWORD")  # Replace with your app password
-app.config['MAIL_DEFAULT_SENDER'] = 'beno@gmail.com'
+
 mail = Mail(app)
+
+
 
 # Routes for navigating through the form
 @app.route('/')
